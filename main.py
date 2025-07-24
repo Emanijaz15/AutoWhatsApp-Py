9b1fae566521365df3ce78bca13290d075bdc508
Emanijaz15 <eijaz7357@gmail.com>
2025-07-24 19:59:22 +0500

    commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import pywhatkit
+
+
+pywhatkit.sendwhatmsg(
+    phone_no="+92**********",    #paste number here
+    message="Hello! This is done by python",
+    time_hour=19,      #it will be sent at 7:47 PM
+    time_min=47                 
+)
